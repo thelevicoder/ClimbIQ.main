455bc4ffc7009e60616faf7129f32094bf0a762d
Brendan Bessman <bdbessman@gmail.com>
2025-02-12 21:26:21 -0800

    adds new login UI

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,19 @@
-import TextField from '@mui/material/TextField';
-import { Typography } from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { useNavigate, Link } from 'react-router-dom';
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useNavigate, Link } from "react-router-dom";
 import React, { useState, useEffect, useContext } from "react";
 import { API_URL } from "../utils";
-import HttpsIcon from '@mui/icons-material/Https';
-import PersonIcon from '@mui/icons-material/Person';
-import InputAdornment from '@mui/material/InputAdornment';
-import { UserContext } from '../Contexts/UserContext';
+import HttpsIcon from "@mui/icons-material/Https";
+import PersonIcon from "@mui/icons-material/Person";
+import InputAdornment from "@mui/material/InputAdornment";
+import { UserContext } from "../Contexts/UserContext";
 
 // Define the LoginForm component
 export const LoginForm = () => {
   const { setCurrentUser, currentUser } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState(""); 
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
@@ -25,13 +25,16 @@ export const LoginForm = () => {
     try {
       console.log("Fetching user:", email);
 
-      const response = await fetch(`${API_URL}/user?email=${encodeURIComponent(email)}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        mode: "cors",
-      });
+      const response = await fetch(
+        `${API_URL}/user?email=${encodeURIComponent(email)}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          mode: "cors",
+        }
+      );
 
       if (!response.ok) {
         console.error(`HTTP error! Status: ${response.status}`);
@@ -61,7 +64,7 @@ export const LoginForm = () => {
     if (currentUser && Object.keys(currentUser).length > 0) {
       console.log("User found:", currentUser);
       if (password === currentUser.password) {
-        navigate('/');
+        navigate("/");
       } else {
         console.log("Invalid email or password");
         setError(true);
@@ -71,78 +74,124 @@ export const LoginForm = () => {
 
   // Page Body
   return (
-    <div className="loginBody">
-      <div className='rectangle'>
-        <Typography align='center'>
-          <div className="logo-container">
-            <img src="ClimbIQ_logo.png" alt="ClimbIQ Logo" className="logo" />
-            <span className="logo-text">ClimbIQ™</span>
+    <div className="backgroundimage">
+      <div className="loginBody">
+        <div className="rectangle">
+          <Typography align="center">
+            <div className="logo-container">
+              <img src="newlogo.png" alt="ClimbIQ Logo" className="logo" />
+              <img src="climbiq-textlogo.png" className="logo-text" />
+            </div>
+          </Typography>
+          <Typography
+            align="left"
+            variant="h6"
+            paddingTop={2}
+            paddingBottom={2}
+            fontSize="14px"
+            color="#002A22"
+          >
+            <span className="tinytext">Login to your ClimbIQ Account</span>
+          </Typography>
+          <div className="userForm">
+            <TextField
+              fullWidth
+              size="small"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: 10,
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#095043", // Change this to your desired focus color
+                  },
+                },
+                "& input:-webkit-autofill": {
+                  WebkitBoxShadow: "0 0 0 1000px #D9D6C0 inset",
+                  WebkitTextFillColor: "#002A22", // Change this to your desired color
+                },
+              }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+              }}
+              id="email"
+              label="Email"
+              variant="outlined"
+              error={error}
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setError(false);
+              }}
+              name="email"
+            />
           </div>
-        </Typography>
-        <Typography align="left" variant="h6" paddingTop={2} paddingBottom={2} fontSize='1.2vw'>
-          Login to your ClimbIQ™ Account
-        </Typography>
-        <div className='userForm'>
-          <TextField
+          <div className="userForm">
+            <TextField
+              fullWidth
+              size="small"
+              sx={{
+                "& .MuiOutlinedInput-root": { borderRadius: 10,
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#095043", // Change this to your desired focus color
+                  },
+                 },
+                "& input:-webkit-autofill": {
+                  WebkitBoxShadow: "0 0 0 1000px #D9D6C0 inset",
+                  WebkitTextFillColor: "#002A22", // Change this to your desired color
+                },
+              }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <HttpsIcon />
+                  </InputAdornment>
+                ),
+              }}
+              type="password"
+              id="password"
+              label="Password"
+              variant="outlined"
+              error={error}
+              helperText={error ? "Invalid email or password" : ""}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError(false);
+              }}
+              name="password"
+            />
+          </div>
+          <LoadingButton
             fullWidth
-            size='small'
-            sx={{ '& .MuiOutlinedInput-root': { borderRadius: 10 } }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <PersonIcon />
-                </InputAdornment>
-              ),
+            loading={isLoading}
+            size="big"
+            disabled={!email.length || !password.length}
+            variant="contained"
+            onClick={findUser}
+            color="error"
+            sx={{
+              paddingTop: "2",
+              borderRadius: 5,
+              backgroundColor: "#095043", // Change this to your desired color
+              "&:hover": {
+                backgroundColor: "#002A22", // Change this to your desired hover color
+              },
             }}
-            id="email"
-            label="Email"
-            variant="outlined"
-            error={error}
-            value={email}
-            onChange={(e) => { setEmail(e.target.value); setError(false); }}
-            name='email'
-          />
-        </div>
-        <div className='userForm'>
-          <TextField
-            fullWidth
-            size='small'
-            sx={{ '& .MuiOutlinedInput-root': { borderRadius: 10 } }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <HttpsIcon />
-                </InputAdornment>
-              ),
-            }}
-            type='password'
-            id="password"
-            label="Password"
-            variant="outlined"
-            error={error}
-            helperText={error ? "Invalid email or password" : ""}
-            value={password}
-            onChange={(e) => { setPassword(e.target.value); setError(false); }}
-            name='password'
-          />
-        </div>
-        <LoadingButton
-          fullWidth
-          loading={isLoading}
-          size='big'
-          disabled={!email.length || !password.length}
-          variant='contained'
-          onClick={findUser}
-          sx={{ paddingTop: "2", borderRadius: 5 }}
-        >
-          SIGN IN
-        </LoadingButton>
-        <div className='tinytext'>
-          <br />
-          By logging into your ClimbIQ™ account, you accept all of the ClimbIQ™ terms and conditions
-        </div>
-        <div className='tinytext'>
-          Don't have an account? Create one <Link to='/create-account'>here</Link>
+          >
+            SIGN IN
+          </LoadingButton>
+          <div className="tinytext">
+            <br />
+            By logging into your ClimbIQ account, you accept all of the ClimbIQ
+            terms and conditions
+          </div>
+          <div className="tinytext">
+            Don't have an account? Create one{" "}
+            <Link to="/create-account">here</Link>
+          </div>
         </div>
       </div>
     </div>
